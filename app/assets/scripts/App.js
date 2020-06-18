e8a9118d690faf2de4dc6a53fa5d181295d68337
Octavian <octavian4ik.mitu@gmail.com>
2020-06-18 12:33:05 +0300

    Reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,12 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 0.75);
+new RevealOnScroll(document.querySelectorAll('.testimonial'), .6);
 
 let mobileMenu = new MobileMenu();
+
 
 if ( module.hot ) {
     module.hot.accept();
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,51 @@
+import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
+
+class RevealOnScroll {
+    constructor(items, threshold) {
+        this.threshold = threshold;
+        this.itemsToReveal = items;
+        this.browserHeight = window.innerHeight;
+        this.hideInitially();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+        this.events();
+    }
+
+    events() {
+        window.addEventListener('scroll', this.scrollThrottle);
+        window.addEventListener('resize', debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 333))
+    }
+
+    calcCaller() {
+        this.itemsToReveal.forEach(el => {
+            if ( !el.isRevealed ) {
+                this.calculateIfScrolledTo(el);
+            }
+        })
+    }
+
+    hideInitially() {
+        this.itemsToReveal.forEach(el => {
+            el.classList.add('reveal-item');
+            el.isRevealed = false;
+        });
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
+    }
+
+    calculateIfScrolledTo(el) {
+        if ( window.scrollY + this.browserHeight > el.offsetTop ) {
+            let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight);
+            if ( scrollPercent < this.threshold ) {
+                el.classList.add('reveal-item--is-visible');
+                el.isRevealed = true;
+                if ( el.isLastItem ) {
+                    window.removeEventListener('scroll', this.scrollThrottle);
+                }
+            }
+        }
+    }
+}
+
+export default RevealOnScroll;
