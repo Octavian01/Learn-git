19e1381c71234798e31191a6c10806cfecd997c7
Octavian <octavian4ik.mitu@gmail.com>
2020-06-19 18:31:11 +0300

    Modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,8 +6,26 @@ import StickHeader from './modules/StickyHeader';
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 0.75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), .6);
 new StickHeader();
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if ( !modal ) {
+            import('./modules/Modal')
+            .then(x => {
+                modal = new x.default();
+                modal.openTheModal();
+            })
+            .catch(err => {
+                throw err;
+            })
+        } else {
+            modal.openTheModal();
+        }
+    })
+})
 
 
 if ( module.hot ) {
